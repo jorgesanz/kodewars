62f6e5d4ac4a8486be737ea2935e1479bacc7ab3
Jorge Sanz <jorge.sanz@innovation4security.com>
2018-01-07 10:42:56 +0100

    very hard tests added

--- src/main/java/one/kyu/SkyScrappers7.java
+++ src/main/java/one/kyu/SkyScrappers7.java
@@ -32,11 +32,53 @@ public class SkyScrappers7 {
 
 
     @Test
+    public void solvePuzzle_VeryHard1() {
+
+        long initTime = System.currentTimeMillis();
+
+        int[] clues = new int[]{ 0,0,5,0,0,0,6,4,0,0,2,0,2,0,0,5,2,0,0,0,5,0,3,0,5,0,0,3};
+        int[][] expected = new int[][]{ new int[] { 7,6,2,1,5,4,3 },
+                new int[] { 1,3,5,4,2,7,6 },
+                new int[] { 6,5,4,7,3,2,1 },
+                new int[] { 5,1,7,6,4,3,2 },
+                new int[] { 4,2,1,3,7,6,5 },
+                new int[] { 3,7,6,2,1,5,4 },
+                new int[] { 2,4,3,5,6,1,7 }};
+
+        int[][] actual = SkyScrappers6.solvePuzzle(clues);
+
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("solution in %s milliseconds", endTime - initTime));
+        //assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void solvePuzzle_VeryHard2() {
+
+        long initTime = System.currentTimeMillis();
+
+        int[] clues = new int[]{ 0,0,5,3,0,2,0,0,0,0,4,5,0,0,0,0,0,3,2,5,4,2,2,0,0,0,0,5 };
+        int[][] expected = new int[][]{ new int[] { 7,6,2,1,5,4,3 },
+                new int[] { 1,3,5,4,2,7,6 },
+                new int[] { 6,5,4,7,3,2,1 },
+                new int[] { 5,1,7,6,4,3,2 },
+                new int[] { 4,2,1,3,7,6,5 },
+                new int[] { 3,7,6,2,1,5,4 },
+                new int[] { 2,4,3,5,6,1,7 }};
+
+        int[][] actual = SkyScrappers6.solvePuzzle(clues);
+
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("solution in %s milliseconds", endTime - initTime));
+        //assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
     public void solvePuzzle_VeryHard3() {
 
         long initTime = System.currentTimeMillis();
-        int[] clues = new int[]{ 0,2,3,0,2,0,0, 5,0,4,5,0,4,0, 0,4,2,0,0,0,6, 5,2,2,2,2,4,1 };      // NOTE: for a VERY HARD puzzle, replace the last 7 values with zeroes
-
+        int[] clues = new int[]{ 0,2,3,0,2,0,0, 5,0,4,5,0,4,0, 0,4,2,0,0,0,6, 5,2,2,2,2,4,1 };
         int[][] expected = new int[][]{ new int[] { 7,6,2,1,5,4,3 },
                 new int[] { 1,3,5,4,2,7,6 },
                 new int[] { 6,5,4,7,3,2,1 },
@@ -56,8 +98,7 @@ public class SkyScrappers7 {
     public void solvePuzzle_VeryHard4() {
 
         long initTime = System.currentTimeMillis();
-        int[] clues = new int[]{ 0,2,3,0,2,0,0, 5,0,4,5,0,4,0, 0,4,2,0,0,0,6, 0,0,0,0,0,0,0 };      // NOTE: for a VERY HARD puzzle, replace the last 7 values with zeroes
-
+        int[] clues = new int[]{ 0,2,3,0,2,0,0, 5,0,4,5,0,4,0, 0,4,2,0,0,0,6, 0,0,0,0,0,0,0 };
         int[][] expected = new int[][]{ new int[] { 7,6,2,1,5,4,3 },
                 new int[] { 1,3,5,4,2,7,6 },
                 new int[] { 6,5,4,7,3,2,1 },
